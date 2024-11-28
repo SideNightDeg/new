0d43016a0a3b488d5406997b82dee5efc503e5a7
Maksim <maksim.pim2725@gmail.com>
2024-11-28 16:20:28 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-alert("Hello")
+
         document.querySelector('.head_burger').addEventListener('click',function(){
           document.querySelector('.nav_').classList.toggle('open');
         })
